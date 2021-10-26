8f975c727974d9bc726aac38cc27a275a07c2a46
Ademir Oliveira <ademir.sojunior@gmail.com>
2021-10-26 15:47:58 -0300

    valores dos salarios corrigidos

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,9 +6,9 @@ fun main(args: Array<String>) {
 
     if (vendas > 10000){
         println("Sua comissão é de: ${(vendas * 30)/100}")
-    }else if (vendas >= 5001.0 && vendas <= 9999.0){
+    }else if (vendas >= 5000.0 && vendas <= 9999.99){
         println("Sua comissão é de: ${(vendas * 20)/100}")
-    }else if (vendas >= 1001.0 && vendas <= 4999.0){
+    }else if (vendas >= 1000.0 && vendas <= 4999.99){
         println("Sua comissão é de: ${(vendas * 10)/100}")
     }else
         println("N/D - Sem comissão")
